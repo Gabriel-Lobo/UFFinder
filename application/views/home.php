1afa03ac2282d81f7c322ed746f150bdcd56409d
Gabriel Lobo <Gabriel-Lobo@users.noreply.github.com>
2019-05-18 18:06:02 -0300

    change the way the header and footer are included in home.php it is now loaded from Home controller instead of being loaded by default php 'include'. update application/controllers/Home.php and application/views/home.php.

--- application/views/home.php
+++ application/views/home.php
@@ -1,25 +1,18 @@
-	<?php include 'header.php';?>
+<div id='main_container'>
+	<div id='search_container'>
+		<h2>Encontre sua Sala</h2>
+		<form autocomplete='off' id='search_form' action='results.php' method='post'>
+ 			<div class='autocomplete'>
 
-	<div id='main_container'>
-		<div id='search_container'>
-			<h2>Encontre sua Sala</h2>
-			<form autocomplete='off' id='search_form' action='results.php' method='post'>
-  			<div class='autocomplete'>
+				<input id='search_input' class='form' type='text' name='search' minlength='3' maxlength='150'>
+				<input type='button' value='Pesquisar'>
 
-					<input id='search_input' class='form' type='text' name='search' minlength='3' maxlength='150'>
-					<input type='button' value='Pesquisar'>
+			</div>
+		</form>
 
-				</div>
-			</form>
+		<div id='classes' class='general_container'></div>
 
-			<div id='classes' class='general_container'></div>
-
-			<h2>ou</h2>
-			<a class='button main_btn' href='classroom.php'>Cadastre uma Sala</a>
-		</div>
+		<h2>ou</h2>
+		<a class='button main_btn' href='classroom.php'>Cadastre uma Sala</a>
 	</div>
-
-	<?php include 'footer.php';?>
-
-</body>
-</html>
+</div>
