c0a319abf2cefe16d34c39d610c2bb06c261af4c
Gabriel Lobo <Gabriel-Lobo@users.noreply.github.com>
2019-05-23 10:24:11 -0300

    major styling overhaul
    This commit changes most of the system's visual identity, mainly it's
    colors. It also replaces underscores for hyphens in all CSS classes
    and remove classes made redundant by bootstrap.

--- application/views/home.php
+++ application/views/home.php
@@ -1,16 +1,15 @@
-<div id='search_container'>
+<div class='inner-container'>
 	<h2>Encontre sua Sala</h2>
-	<form autocomplete='off' id='search_form' action='results.php' method='post'>
+	<form autocomplete='off' id='search-form' action='results.php' method='post'>
 		<div class='autocomplete'>
 
-			<input id='search_input' class='form' type='text' name='search' minlength='3' maxlength='150'>
-			<input type='button' value='Pesquisar'>
+			<input id='search-input' type='text' name='search' minlength='3' maxlength='150'>
+			<input class='btn search-button' type='button' value='Pesquisar'>
 
 		</div>
 	</form>
 
-	<div id='classes' class='general_container'></div>
+	<div id='classes' class='general-container'></div>
 
-	<h2>ou</h2>
-	<a class='button main_btn' href='classroom.php'>Cadastre uma Sala</a>
+	<a class='btn btn-main' href='classroom.php'>Cadastre uma Sala</a>
 </div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,5 @@
 	<div id='footer'>
-		<div id='footer_logo'>
+		<div id='footer-logo'>
 		</div>
 	</div>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
