02632ff11eeb46927f5d7a26d31d520a6a3f0c57
Gabriel Lobo <gabrielteixeiralobo@gmail.com>
2019-12-02 14:49:18 -0300

    add jquery.js and popper.js for bootstrap dependencies

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,12 +1,17 @@
-	<div id='footer'>
-		<div id='footer-logo'>
+		<div id='footer'>
+			<div id='footer-logo'></div>
 		</div>
-	</div>
-	<script type="text/javascript">
-    var CFG = {
-        url: '<?php echo $this->config->item('base_url');?>',
-        token: '<?php echo $this->security->get_csrf_hash();?>'
-    };
-	</script>
-</body>
+
+		<script type="text/javascript">
+			var CFG = {
+				url: '<?php echo $this->config->item('base_url');?>',
+				token: '<?php echo $this->security->get_csrf_hash();?>'
+			};
+		</script>
+
+		<script src="<?php echo assets_url(); ?>js/jquery.min.js"></script>
+		<script src="<?php echo assets_url(); ?>js/popper.min.js"></script>
+		<script src="<?php echo assets_url(); ?>js/bootstrap.min.js"></script>
+
+	</body>
 </html>
