cda57b679b34ecb3dd1788c378599d2e26942250
Gabriel Lobo <gabrielteixeiralobo@gmail.com>
2020-12-13 12:50:40 -0300

    update bootstrap to version 5.0.0 beta
    Now served through CDN. Removed deprecated jQuery dependency.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,7 @@
 	<div id='footer'>
 		<div id='footer-logo'>
 		</div>
+<<<<<<< Updated upstream
 	</div>
 	<script type="text/javascript">
     var CFG = {
@@ -9,4 +10,17 @@
     };
 	</script>
 </body>
+=======
+
+		<script type="text/javascript">
+			var CFG = {
+				url: '<?php echo $this->config->item('base_url');?>',
+				token: '<?php echo $this->security->get_csrf_hash();?>'
+			};
+		</script>
+
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+	</body>
+>>>>>>> Stashed changes
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<title>UFFinder</title>
 	<meta charset='UTF-8'>
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
-	<link rel="stylesheet" href="<?php echo assets_url(); ?>css/bootstrap.min.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?php echo assets_url(); ?>css/all.css">
 	<link rel='stylesheet' type="text/css" href='<?php echo assets_url(); ?>css/style.css'>
 </head>
